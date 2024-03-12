0cedd7dbb3a7b0702989899877e4a352c48b0ab2
Thomas Kraak <thomas.kraak@capgemini.com>
2024-03-12 12:57:42 +0100

    First commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from "@angular/router";
+import { HomepageComponent } from "./pages/homepage/homepage.component";
+import { NgModule } from "@angular/core";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    component: HomepageComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
