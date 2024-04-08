331e7fb08e0f55bb130ed241925a1801a0073939
Thomas Kraak <thomas.kraak@capgemini.com>
2024-04-08 20:40:24 +0200

    Removed contact component

--- src/app/pages/contactpage/contactpage.component.ts
+++ src/app/pages/contactpage/contactpage.component.ts
@@ -1,11 +1,10 @@
 import { Component } from "@angular/core";
 import { CardComponent } from "../../components/card/card.component";
-import { ContactComponent } from "../../components/contact/contact.component";
 
 @Component({
   selector: "contactpage",
   standalone: true,
-  imports: [CardComponent, ContactComponent],
+  imports: [CardComponent],
   templateUrl: "./contactpage.component.html",
   styleUrl: "./contactpage.component.scss"
 })
